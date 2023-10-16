61279385c0909036ab8e9b37656a0904c35975ed
CanvasL <746591811@qq.com>
2023-10-16 10:44:52 +0800

    doc: readme

--- src/contents/all/index.tsx
+++ src/contents/all/index.tsx
@@ -23,28 +23,27 @@ window.addEventListener('message', async (event) => {
 
 const script = document.createElement('script');
 script.textContent = `
-    const EXTENSION_ID = "abbdlfdjkoideiedlmilklpfcojbbfnm";
     const getCurrentDate = () => {
         const now = new Date();
         const year = now.getUTCFullYear();
         const month = now.getUTCMonth() + 1; // Months are zero-based
         const day = now.getUTCDate();
-    
+
         return year + '-' + month + '-' + day;
-    }
+    };
 
     window.attentionflow = {
-        getAttentionRecord: async function(date = getCurrentDate()) {
-            window.postMessage({ type: 'FROM_INJECTED_SCRIPT', payload: {date: date} }, '*');
+        getAttentionRecord: async function (date = getCurrentDate()) {
+            window.postMessage({ type: 'FROM_INJECTED_SCRIPT', payload: { date: date } }, '*');
             return new Promise((resolve) => {
-                window.addEventListener('message', function(event) {
+                window.addEventListener('message', function (event) {
                     if (event.source === window && event.data.type === 'FROM_CONTENT_SCRIPT') {
-                      resolve(event.data.payload);
+                        resolve(event.data.payload);
                     }
                 });
             });
-          },
-      };
-  `;
+        },
+    };
+`;
 (document.head || document.documentElement).appendChild(script);
 script.remove();
